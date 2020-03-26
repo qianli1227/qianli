0f7aac55155ff0e8fde009d952b1165de1037ae4
apple <qianli@ruhnn.com>
2020-03-26 15:31:52 +0800

    提交

--- src/main/java/com/ql/java/base/ThreadTest.java
+++ src/main/java/com/ql/java/base/ThreadTest.java
@@ -1,28 +1,31 @@
 package com.ql.java.base;
 
 public class ThreadTest {
-   public static void main(String[] args){
 
-       method1 method1=new method1();
-       method1.start();
-       for (int i=0;i<100;i++){
-           System.out.println(Thread.currentThread().getName()+":"+i);
-       }
-   }
 
+    public static void main(String[] args){
+
+        method1 method1=new method1();
+        method1.start();
+        for (int i=0;i<100;i++){
+            System.out.println(method1.currentThread().getName()+":"+i);
+        }
+    }
 
 
 }
-
-  class method1 extends Thread{
-      @Override
-      public void run() {
+class method1 extends Thread{
+    @Override
+    public void run() {
         for (int i=0;i<100;i++){
             if (i%2==0){
-                currentThread().setName("线程方式1");
+                Thread.currentThread().setName("线程方式1");
                 System.out.println(currentThread().getName()+":"+i);
             }
         }
-      }
-  }
+    }
+}
+
+
+
 

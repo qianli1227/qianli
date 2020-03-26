c9858baf65ea60b68a7a9a81a36027b494a6cce5
apple <qianli@ruhnn.com>
2020-03-26 15:37:53 +0800

    提交

--- src/main/java/com/ql/java/base/ThreadTest.java
+++ src/main/java/com/ql/java/base/ThreadTest.java
@@ -8,7 +8,7 @@ public class ThreadTest {
         method1 method1=new method1();
         method1.start();
         for (int i=0;i<100;i++){
-            System.out.println(method1.currentThread().getName()+":"+i);
+            System.out.println(Thread.currentThread().getName()+":"+i);
         }
     }
 
@@ -20,7 +20,7 @@ class method1 extends Thread{
         for (int i=0;i<100;i++){
             if (i%2==0){
                 Thread.currentThread().setName("线程方式1");
-                System.out.println(currentThread().getName()+":"+i);
+                System.out.println(method1.currentThread().getName()+":"+i);
             }
         }
     }
